5d00a49ff85d6399fc19315b043723baa5f12476
Grace Fallon <graceleefallon@gmail.com>
2021-12-06 22:15:19 -0600

    changed from tracking each fish to tracking cycle brackets with IEnum/switch statements

--- day_6-lanternfish/day_6-lanternfish/day_6-lanternfish.cs
+++ day_6-lanternfish/day_6-lanternfish/day_6-lanternfish.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace day_6_lanternfish
 {
@@ -7,13 +8,22 @@ namespace day_6_lanternfish
     {
         static void Main(string[] args)
         {
-            List<int> lanternfishData = ParseInput(InputString());
+            Dictionary<long, long> lanternfishData = ParseInput(InputString());
 
-            // hard coded for 256 days for the problem, previously was 80
-            // should be dynamic
-            for (int day = 0; day < 256; day++)
+            long sum = 0;
+            foreach (long value in LanternfishReproductionCycleStatus(lanternfishData).Skip(255).First().Values)
             {
-                int newLanternfishCount = 0;
+                Console.WriteLine(value);
+                sum = (long)sum + (long)value;
+            }
+            Console.WriteLine(sum);
+
+        // the commented code below is the brute force method,
+        // as population grew exponentially, tracking became better implemented
+        // by bracket, rather than by individual lanternfish
+
+        /*  for (int day = 0; day < 256; day++)
+            {
                 // iterate through lantern fish, checking for spawning
                 for (int x = 0; x < lanternfishData.Count; x++)
                 {
@@ -33,18 +43,69 @@ namespace day_6_lanternfish
                 {
                     lanternfishData.Add(8);
                 }
-            }
-            Console.WriteLine(lanternfishData.Count);
+            } */
         }
-        public static List<int> ParseInput(string inputString)
+
+        public static IEnumerable<Dictionary<long, long>> LanternfishReproductionCycleStatus(Dictionary<long, long> parsedInputDict)
         {
-            List<int> parsedList = new List<int>();
+            while(true)
+            {
+                long newLanternfish = 0;
+                long newCycleLanternfish = 0;
+                for (int key = 0; key <= 8; key++)
+                {
+                    // switch statement with cases for each potential point in the cycle
+                    switch (key)
+                    {
+                        case 0:
+                            newLanternfish = parsedInputDict[0];
+                            newCycleLanternfish = parsedInputDict[0];
+                            parsedInputDict[0] = parsedInputDict[1];
+                            break;
+                        case 1:
+                            parsedInputDict[1] = parsedInputDict[2];
+                            break;
+                        case 2:
+                            parsedInputDict[2] = parsedInputDict[3];
+                            break;
+                        case 3:
+                            parsedInputDict[3] = parsedInputDict[4];
+                            break;
+                        case 4:
+                            parsedInputDict[4] = parsedInputDict[5];
+                            break;
+                        case 5:
+                            parsedInputDict [5] = parsedInputDict[6];
+                            break;
+                        case 6:
+                            parsedInputDict[6] = newCycleLanternfish + parsedInputDict[7];
+                            break;
+                        case 7:
+                            parsedInputDict[7] = parsedInputDict[8];
+                            break;
+                    }
+                }
+                parsedInputDict[8] = newLanternfish;
+                yield return parsedInputDict;
+            }
+        }
+
+        public static Dictionary<long, long> ParseInput(string inputString)
+        {
+            Dictionary<long, long> lanternFishCount = new Dictionary<long, long>()
+            {
+                {0, 0},{1, 0},{2, 0},{3, 0},{4, 0},{5, 0},{6, 0},{7, 0},{8, 0}
+            };
             string[] arrayOfDigits = inputString.Split(",");
             foreach (string digit in arrayOfDigits)
             {
-                parsedList.Add(Int32.Parse(digit));
+                lanternFishCount[Int64.Parse(digit)] = lanternFishCount[Int64.Parse(digit)] + 1;
             }
-            return parsedList;
+            foreach (long value in lanternFishCount.Values)
+            {
+                Console.WriteLine(value);
+            }
+            return lanternFishCount;
         }
 
         public static string InputString()
